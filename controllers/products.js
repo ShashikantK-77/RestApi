464902914c7db5acc9b34164de27b8532fecd61d
shashikant kamthe <shashikantkamthe77@gmail.com>
2023-02-27 18:42:47 +0530

    second version

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,59 @@
-const getAllProducts = (req,res) =>{
-res.status(200).json({msg:"i am getAllProducts"});
+const Product = require("../Models/Product");
+
+const getAllProducts = async (req, res) => {
+
+    const { company, name, featured, sort, select } = req.query;
+    const queryObject = {};
+
+    if (company) {
+        queryObject.company = company;
+    }
+
+    if (featured) {
+        queryObject.featured = featured;
+    }
+
+    if (name) {
+
+        queryObject.name = { $regex: name, $options: "i" };
+    }
+
+    let apiData = Product.find(queryObject);
+
+    if (sort) {
+        let sortFix = sort.split(",").join(" ");
+        apiData = apiData.sort(sortFix);
+    }
+
+     // (select = name company;
+  if (select) {
+    // let selectFix = select.replace(",", " ");
+    let selectFix = select.split(",").join(" ");
+    apiData = apiData.select(selectFix);
+  }
+
+  let page = Number(req.query.page) || 1;
+  let limit = Number(req.query.limit) || 3;
+
+  let skip = (page - 1) * limit;
+
+  // page = 2;
+  // limit = 3;
+  // skip =  1 * 3 = 3
+
+  apiData = apiData.skip(skip).limit(limit);
+
+
+    // console.log(queryObject);
+
+    const myData = await apiData;
+    res.status(200).json({ myData, nbHits: myData.length });
 };
 
-const getAllProductsTesting = (req,res) =>{
-    res.status(200).json({msg:"i am getAllProductsTesting"});
-    };
+const getAllProductsTesting = async (req, res) => {
+    const myData = await Product.find(req.query);
 
-    module.exports = {getAllProducts,getAllProductsTesting}
+    res.status(200).json({ myData });
+};
+
+module.exports = { getAllProducts, getAllProductsTesting }
