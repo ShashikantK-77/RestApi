73bfb7d98d7eb3b8d86e600a599cfd426663348e
shashikant kamthe <shashikantkamthe77@gmail.com>
2023-02-28 17:12:27 +0530

    last version

--- controllers/products.js
+++ controllers/products.js
@@ -14,7 +14,6 @@ const getAllProducts = async (req, res) => {
     }
 
     if (name) {
-
         queryObject.name = { $regex: name, $options: "i" };
     }
 
@@ -33,7 +32,7 @@ const getAllProducts = async (req, res) => {
   }
 
   let page = Number(req.query.page) || 1;
-  let limit = Number(req.query.limit) || 3;
+  let limit = Number(req.query.limit) || 10;
 
   let skip = (page - 1) * limit;
 
@@ -46,8 +45,8 @@ const getAllProducts = async (req, res) => {
 
     // console.log(queryObject);
 
-    const myData = await apiData;
-    res.status(200).json({ myData, nbHits: myData.length });
+    const Products = await apiData;
+    res.status(200).json({ Products, nbHits: Products.length });
 };
 
 const getAllProductsTesting = async (req, res) => {
